abb392bc65c003e50e932997fd7da45a3324b9d5
PAULA011 <117386915+PAULA011@users.noreply.github.com>
2022-11-11 14:23:29 -0500

    commit figuras

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+function Circulo(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Circulo');
+}
+
+function Rombo(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Rombo');
+}
+
+function Triangulo() {
+    var figura= document.getElementById('figura');
+    figura.classList.toggle("Triangulo")}
+
+
+
+function arriba(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("top");
+}
+
+function izquierda(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Left");
+}
+
+function abajo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Butom");
+}
+
+function derecha(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Right")
+}
+
+function DiagonalIz(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("DiagonalIz")
+}
+function DiagonalDe(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("DiagonalDe")
+}
+
+function gif(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("gif")
+    }
+
+function Rectangulo(){
+        var figura = document.getElementById('figura')
+        figura.classList.toggle("Rectangulo")
+        }
+
+function Pacman(){
+    var figura = document.getElementById('figura')
+        figura.classList.toggle("Pacman")
+}
+
+
